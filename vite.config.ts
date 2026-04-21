2f37fff00fabc1ac9750e35363fe0b898634737a
yvesjulio <yvesjulio3@gmail.com>
2026-04-21 11:41:41 +0200

    fix: correct vite base path for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/gofixclean-deployingTest/',
+  base: "/",
   plugins: [tailwind(), react()],
   resolve: {
     alias: {
